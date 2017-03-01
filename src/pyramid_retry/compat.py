21ef27a04017aaed6e4a1ef697971856751ce362
Bert JW Regeer <bertjw@regeer.org>
2017-03-01 01:46:36 -0700

    Grab latest copy of reraise from six
    Also removes unused imports

--- src/pyramid_retry/compat.py
+++ src/pyramid_retry/compat.py
@@ -2,34 +2,33 @@ import sys
 
 PY3 = sys.version_info[0] == 3
 
-if PY3:  # pragma: no cover
-    import builtins
-    exec_ = getattr(builtins, "exec")
-
-    text_type = str
-    binary_type = bytes
-
+if PY3:
     def reraise(tp, value, tb=None):
-        if value.__traceback__ is not tb:
-            raise value.with_traceback(tb)
-        raise value
-
-else:  # pragma: no cover
-    def exec_(code, globs=None, locs=None):
+        try:
+            if value is None:  # pragma: nocover
+                value = tp()
+            if value.__traceback__ is not tb:  # pragma: nocover
+                raise value.with_traceback(tb)
+            raise value
+        finally:
+            value = None
+            tb = None
+else:
+    def exec_(_code_, _globs_=None, _locs_=None):
         """Execute code in a namespace."""
-        if globs is None:
+        if _globs_ is None:
             frame = sys._getframe(1)
-            globs = frame.f_globals
-            if locs is None:
-                locs = frame.f_locals
+            _globs_ = frame.f_globals
+            if _locs_ is None:
+                _locs_ = frame.f_locals
             del frame
-        elif locs is None:
-            locs = globs
-        exec("""exec code in globs, locs""")
-
-    text_type = unicode
-    binary_type = str
+        elif _locs_ is None:  # pragma: nocover
+            _locs_ = _globs_
+        exec("""exec _code_ in _globs_, _locs_""")
 
     exec_("""def reraise(tp, value, tb=None):
-    raise tp, value, tb
+    try:
+        raise tp, value, tb
+    finally:
+        tb = None
 """)
