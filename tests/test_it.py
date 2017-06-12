b5c8edf907a7ae5742cd80f79911352869695b4d
Michael Merickel <michael@merickel.org>
2017-06-11 23:48:39 -0500

    add a BeforeRetry event

--- tests/test_it.py
+++ tests/test_it.py
@@ -71,6 +71,7 @@ def test_raising_nonretryable_is_not_caught(config):
         app.get('/')
     assert calls == ['fail']
 
+
 def test_handled_error_is_retried(config):
     from pyramid_retry import RetryableException
     calls = []
@@ -93,6 +94,7 @@ def test_handled_error_is_retried(config):
     assert response.body == b'default'
     assert calls == ['fail', 'caught', 'fail', 'caught', 'fail', 'default']
 
+
 def test_retryable_exception_is_ignored_on_last_attempt(config):
     from pyramid_retry import RetryableException
     calls = []
@@ -105,6 +107,59 @@ def test_retryable_exception_is_ignored_on_last_attempt(config):
     with pytest.raises(Exception):
         app.get('/')
     assert calls == ['fail', 'fail', 'fail']
+
+
+def test_BeforeRetry_event_is_raised(config):
+    from pyramid_retry import RetryableException
+    from pyramid_retry import IBeforeRetry
+    calls = []
+    retries = []
+    def retry_subscriber(event):
+        retries.append(event)
+    def final_view(request):
+        calls.append('ok')
+        return 'ok'
+    def bad_view(request):
+        calls.append('fail')
+        raise RetryableException
+    config.add_subscriber(retry_subscriber, IBeforeRetry)
+    config.add_view(bad_view, last_retry_attempt=False)
+    config.add_view(final_view, last_retry_attempt=True, renderer='string')
+    app = config.make_wsgi_app()
+    app = webtest.TestApp(app)
+    response = app.get('/')
+    assert response.body == b'ok'
+    assert calls == ['fail', 'fail', 'ok']
+    assert len(retries) == 2
+
+
+def test_BeforeRetry_event_is_raised_from_squashed_exception(config):
+    from pyramid_retry import IBeforeRetry
+    from pyramid_retry import RetryableException
+    calls = []
+    retries = []
+    def retry_subscriber(event):
+        retries.append(event)
+    def final_view(request):
+        calls.append('ok')
+        return 'ok'
+    def bad_view(request):
+        raise RetryableException
+    def exc_view(request):
+        calls.append('squash')
+        return 'squash'
+    config.add_subscriber(retry_subscriber, IBeforeRetry)
+    config.add_view(bad_view, last_retry_attempt=False)
+    config.add_view(exc_view, context=RetryableException, exception_only=True,
+                    renderer='string')
+    config.add_view(final_view, last_retry_attempt=True, renderer='string')
+    app = config.make_wsgi_app()
+    app = webtest.TestApp(app)
+    response = app.get('/')
+    assert response.body == b'ok'
+    assert calls == ['squash', 'squash', 'ok']
+    assert len(retries) == 2
+
 
 def test_activate_hook_overrides_default_attempts(config):
     from pyramid_retry import RetryableException
@@ -125,6 +180,7 @@ def test_activate_hook_overrides_default_attempts(config):
         app.get('/')
     assert calls == ['fail']
 
+
 def test_activate_hook_falls_back_to_default_attempts(config):
     from pyramid_retry import RetryableException
     calls = []
@@ -144,10 +200,12 @@ def test_activate_hook_falls_back_to_default_attempts(config):
         app.get('/')
     assert calls == ['fail', 'fail', 'fail']
 
+
 def test_is_last_attempt_True_when_inactive():
     from pyramid_retry import is_last_attempt
     request = pyramid.request.Request.blank('/')
     assert is_last_attempt(request)
+
 
 def test_retryable_error_predicate_is_bool(config):
     from pyramid.exceptions import ConfigurationError
@@ -155,6 +213,7 @@ def test_retryable_error_predicate_is_bool(config):
     with pytest.raises(ConfigurationError):
         config.add_view(view, retryable_error='yes', renderer='string')
         config.commit()
+
 
 def test_last_retry_attempt_predicate_is_bool(config):
     from pyramid.exceptions import ConfigurationError
